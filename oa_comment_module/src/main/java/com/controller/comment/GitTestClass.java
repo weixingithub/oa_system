3a43b756eb58f54e9f88ca90cd0cf09525338e67
weixin <763328075@qq.com>
2017-05-14 17:44:21 +0800

    生成setergeter方法

--- oa_comment_module/src/main/java/com/controller/comment/GitTestClass.java
+++ oa_comment_module/src/main/java/com/controller/comment/GitTestClass.java
@@ -3,4 +3,16 @@ package com.controller.comment;
 public class GitTestClass {
      private String testName;
      private Integer gitAge;
+	public String getTestName() {
+		return testName;
+	}
+	public void setTestName(String testName) {
+		this.testName = testName;
+	}
+	public Integer getGitAge() {
+		return gitAge;
+	}
+	public void setGitAge(Integer gitAge) {
+		this.gitAge = gitAge;
+	}
 }
